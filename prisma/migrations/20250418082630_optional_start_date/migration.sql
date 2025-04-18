d714f313cddc313b681b2bed4152dd8a3554d698
One-Zee <harunadindane15@gmail.com>
2025-04-18 10:32:18 +0200

    chore: update start_date to optional

---
+++ prisma/migrations/20250418082630_optional_start_date/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "Course" ALTER COLUMN "startDate" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Education" ALTER COLUMN "startDate" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Experience" ALTER COLUMN "startDate" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Project" ALTER COLUMN "startDate" DROP NOT NULL;
