971bde152175bfa4edacc1866ef9a38bc2fba876
One-Zee <harunadindane15@gmail.com>
2025-04-28 10:45:27 +0200

    refactor: remove unused imports and commented-out getHello method in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,6 @@
-import { Controller, Get } from "@nestjs/common";
-import { ApiOperation } from "@nestjs/swagger";
-import { addMonths, format } from "date-fns";
+import { Controller } from "@nestjs/common";
 
 @Controller()
 export class AppController {
   constructor() {}
-
-  @Get("hello")
-  @ApiOperation({
-    summary: "Get Hello",
-    description: "Get Hello",
-  })
-  async getHello(): Promise<string> {
-    let a = addMonths(new Date(), 1);
-
-    console.log(a);
-
-    return "Hello World";
-  }
 }
